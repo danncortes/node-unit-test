0ee9c626435a9a07cd6da4766700d420efaeda36
Daniel Cortes <danncortes@gmail.com>
2018-01-24 22:50:16 -0500

    ending testing

--- tests/constructor.test.js
+++ tests/constructor.test.js
@@ -1,7 +1,27 @@
 var assert = require('chai').assert;
+var should = require('chai').should();
 const Person = require('../constructor');
 
-it('Should create an person object', () => {
-    var worker = new Person('Daniel', 37, 'developer', 'Venezuela');
-    assert.typeOf(worker, 'object');
+const worker = new Person('Daniel', 37, 'developer', 'Venezuela');
+
+describe('Person Class', () => {
+    it('Should create an person object', () => {
+        assert.typeOf(worker, 'object');
+    });
+
+    it('Should has a name property', () => {
+        worker.should.include.keys('name');
+    })
+
+    it('Should has a age property', () => {
+        worker.should.include.keys('age');
+    });
+
+    it('Should has an occupation property', () => {
+        worker.should.include.keys('occupation');
+    });
+
+    it('Should has a greeting method', () => {
+        assert.typeOf(worker.greeting, 'function');
+    });
 });
